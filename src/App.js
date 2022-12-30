edb310c950a1d2718a094d6df8b7e0bebdf6eb1c
ST Sabbir <devlopersabbir@gmail.com>
2022-12-30 12:02:03 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
+
+const Menu = () => {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/about">about</Link>
+      <Link to="/contact">contact</Link>
+    </>
+  );
+};
 
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>} />
+        <Route path="about" element={<h1>about page</h1>} />
+        <Route path="contact" element={<h1>contact page</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
