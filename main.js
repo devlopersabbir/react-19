80fedad2c1dc138f21a011c6bfc0e3b5c8a32cb0
Sabbir Hossain Shuvo <sabbir@Sabbirs-MacBook-Air.local>
2024-03-16 22:11:42 +0600

    [chore 😁] updated main.js && package json

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import { simpleGit } from "simple-git";
+
 simpleGit()
-  .add(["./main.js", "./package.json"])
+  .add(["./main.js"])
   .commit("[chore 😁] updated main.js && package json", {
-    "--date": "2024-03-13",
+    "--date": "2024-03-16",
   })
   .push();
