23d424dc9cb107f972830a12739e1019bb8a4ea0
devlopersabbir <devlopersabbir@gmail.com>
2024-03-20 22:18:12 +0600

    [init 🥹] initial commit

--- main.js
+++
@@ -1,8 +0,0 @@
-import { simpleGit } from "simple-git";
-
-simpleGit()
-  .add(["./main.js"])
-  .commit("[chore 😁] updated main.js && package json", {
-    "--date": "2024-04-16",
-  })
-  .push();
